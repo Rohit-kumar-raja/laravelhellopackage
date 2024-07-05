3ecd1a5bfa69b7c16311d6fbb2fce68d847d8193
rohit kumar <rohitk.techexactly@gmail.com>
2024-07-05 12:27:06 +0530

    all done

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rohit/hellopackage',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bfdb6551ad7c832bcbea062a9e731b297070eaaf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rohit/hellopackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bfdb6551ad7c832bcbea062a9e731b297070eaaf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
